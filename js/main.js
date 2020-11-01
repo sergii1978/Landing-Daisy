8ade2e415b4685f98c618637578588eed5af5755
Sergii Storcheus <44782756+sergii1978@users.noreply.github.com>
2020-11-01 17:09:18 +0200

    add slider owl

--- js/main.js
+++ js/main.js
@@ -1,4 +1,23 @@
 var mixer = mixitup ( '.container-filter' );
 
 // slick slider
-$('.single-item').slick();
+// $('.single-item').slick();
+
+// owl carousel
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    dots: false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
