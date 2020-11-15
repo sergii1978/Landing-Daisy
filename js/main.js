9dc1dd0a4af302b0a834ec07f8b02ee869045254
Sergii Storcheus <44782756+sergii1978@users.noreply.github.com>
2020-11-15 17:36:37 +0200

    add menu JS

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
+// portfolio-slider
 var mixer = mixitup ( '.container-filter' );
 
-// swiper
+// swiper-carousel
             var mySwiper = new Swiper('.swiper-container', {
         // Optional parameters
         direction: 'horizontal',
@@ -13,3 +14,30 @@ var mixer = mixitup ( '.container-filter' );
           prevEl: '.swiper-button-prev',
         },
       })
+
+//Выпадающее меню
+$(document).ready(function() {
+      $('.menu-icon').click(function () {
+      $('.nav-hide').slideToggle(500);
+      $('.nav-hide').css({
+          'display': 'flex',
+          'justify-content': 'center'
+      });
+      $('ul.header-menu').css({
+          'display': 'flex',
+          'flex-direction': 'column',
+          'justify-content': 'center',
+          'background': 'transparent'
+      });
+      $('.header-menu li').css({
+          'margin-left': '0',
+          'margin-top': '5px',
+          'margin-bottom': '5px'
+      });
+
+      if ($('.menu-icon').html() == '<i class="fas fa-bars"></i>') {
+          $(this).html('<i class="fas fa-times"></i>');
+      } else {
+        $(this).html('<i class="fas fa-bars"></i>')};
+  });
+});
